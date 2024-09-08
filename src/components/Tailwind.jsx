3fcddfb39f3660c5567173278e834ecb4f6bbbaa
mBilalVirk <bilalvirk337@gmail.com>
2024-09-08 13:22:08 +0500

    Setup and Typography

--- src/components/Tailwind.jsx
+++ src/components/Tailwind.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 export default function Tailwind() {
   return (
-    <div className="container mx-auto">
-      <h1 className="">Hello</h1>
+    <div>
+      <h1 className="border-4 border-green-600 lg:text-3xl md:text-lg sm:text-3xl mb-1 font-myFont ">
+        Hello World
+      </h1>
+      <h1 className="text-vuejs-900 line-through ">Hello World</h1>
+      <h1 className="bg-[#e76652]">Hello World</h1>
+      <h1 className="text-4xl underline decoration-wavy decoration-red-400 underline-offset-2 leading-loose">
+        Title1
+      </h1>
+      {/* This above heading underline with different decoration you can explor much more */}
+      <h2 className="text-2xl">Title2</h2>
+      <h3 className="text-xl"> Title3</h3>
+      <p className="text-base truncate">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        voluptate, tempore sit accusantium eligendi id? Debitis itaque obcaecati
+        eaque mollitia tenetur, asperiores neque. Sapiente, ipsa.
+      </p>
+      <p className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        voluptate, tempore sit accusantium eligendi id? Debitis itaque obcaecati
+        eaque mollitia tenetur, asperiores neque. Sapiente, ipsa.
+      </p>
+      <p className="text-xs note">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        voluptate, tempore sit accusantium eligendi id? Debitis itaque obcaecati
+        eaque mollitia tenetur, asperiores neque. Sapiente, ipsa.
+      </p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontSize: {
+        base: "18px",
+      },
+      colors: {
+        vuejs: {
+          100: "#49e659",
+          900: "#396359",
+        },
+      },
+      fontFamily: {
+        myFont: ["Protest Guerrilla", "sans-serif"],
+      },
+    },
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
   },
   plugins: [],
 };
