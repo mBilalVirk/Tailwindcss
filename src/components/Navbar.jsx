0ff05e9c785b500f0476eacb9ca962a2217218bf
mBilalVirk <bilalvirk337@gmail.com>
2024-09-10 23:54:01 +0500

    main section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="bg-blue-500 py-3 sticky top-0 transition-all ease-in-out  w-full z-10">
+      <nav className="bg-blue-500 py-3 fixed font-bold -top-1 transition-all ease-in-out  w-full z-10">
         <div className="container mx-auto flex justify-between items-center">
           {/* Logo */}
           <div className="text-white font-bold text-xl ">
@@ -33,7 +33,7 @@ const Navbar = () => {
             </NavLink>
           </div>
           <div>
-            <button className="bg-gradient-to-r hidden md:flex from-purple-500 via-pink-500 to-red-500 text-white py-1 px-6 rounded-full shadow-lg hover:shadow-xl transition-transform transform hover:scale-105 hover:bg-gradient-to-l focus:outline-none focus:ring-4 focus:ring-pink-300">
+            <button className="bg-gradient-to-r hidden md:flex from-purple-500 via-pink-500 to-red-500 text-white py-1 px-6 rounded-full shadow-lg hover:shadow-xl transition-transform transform hover:scale-105 hover:bg-gradient-to-l focus:outline-none focus:ring-4 focus:ring-pink-300 font-bold">
               Login
             </button>
           </div>
@@ -44,27 +44,44 @@ const Navbar = () => {
               onClick={toggleMenu} // Toggle the menu when the button is clicked
               className="text-white focus:outline-none"
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
+              {isOpen ? (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
             </button>
           </div>
         </div>
 
         {/* Mobile Menu (Visible when isOpen is true) */}
         <div
-          className={`md:hidden overflow-hidden fixed bg-blue-500   transition-all duration-300 ease-in-out  left-0 top-[70px] w-screen z-20 ${
+          className={`md:hidden overflow-hidden fixed bg-blue-500   transition-all duration-300 ease-in-out  left-0 top-[70px] w-screen z-20 font-bold ${
             isOpen ? "max-h-40" : "max-h-0"
           }`}
           style={{
@@ -103,7 +120,7 @@ const Navbar = () => {
             </NavLink>
           </div>
           <div className="mt-2 mb-2 space-y-2 container mx-auto justify-items-center">
-            <button className="bg-gradient-to-r md:flex from-purple-500 via-pink-500 to-red-500 text-white py-1 px-6 rounded-full shadow-lg hover:shadow-xl transition-transform transform hover:scale-105 hover:bg-gradient-to-l focus:outline-none focus:ring-4 focus:ring-pink-300">
+            <button className="bg-gradient-to-r md:flex from-purple-500 via-pink-500 to-red-500 text-white py-1 px-6 rounded-full shadow-lg hover:shadow-xl transition-transform transform hover:scale-105 hover:bg-gradient-to-l focus:outline-none focus:ring-4 focus:ring-pink-300 font-bold">
               Login
             </button>
           </div>
